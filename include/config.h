9d11810e44a74813b88be2fb9505e25fdc50d4e8
Simon Ser <contact@emersion.fr>
2019-05-30 15:18:15 +0300

    Parse output keys

--- include/config.h
+++ include/config.h
@@ -1,11 +1,31 @@
 #ifndef KANSHI_CONFIG_H
 #define KANSHI_CONFIG_H
 
-#include <wayland-util.h>
+#include <wayland-client.h>
+
+enum kanshi_output_field {
+	KANSHI_OUTPUT_ENABLED = 1 << 0,
+	KANSHI_OUTPUT_MODE = 1 << 1,
+	KANSHI_OUTPUT_POSITION = 1 << 2,
+	KANSHI_OUTPUT_SCALE = 1 << 3,
+	KANSHI_OUTPUT_TRANSFORM = 1 << 4,
+};
 
 struct kanshi_profile_output {
-	struct wl_list link;
 	char *name;
+	unsigned int fields; // enum kanshi_output_field
+	struct wl_list link;
+
+	bool enabled;
+	struct {
+		int width, height;
+		int refresh; // mHz
+	} mode;
+	struct {
+		int x, y;
+	} position;
+	float scale;
+	enum wl_output_transform transform;
 };
 
 struct kanshi_profile {
--- parser.c
+++ parser.c
@@ -7,10 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <wayland-client.h>
+
 #include "config.h"
 #include "parser.h"
 
-const char *token_type_str(enum kanshi_token_type t) {
+static const char *token_type_str(enum kanshi_token_type t) {
 	switch (t) {
 	case KANSHI_TOKEN_LBRACKET:
 		return "'{'";
@@ -24,7 +26,7 @@ const char *token_type_str(enum kanshi_token_type t) {
 	assert(0);
 }
 
-int parser_read_char(struct kanshi_parser *parser) {
+static int parser_read_char(struct kanshi_parser *parser) {
 	if (parser->next >= 0) {
 		int ch = parser->next;
 		parser->next = -1;
@@ -51,13 +53,13 @@ int parser_read_char(struct kanshi_parser *parser) {
 	return ch;
 }
 
-int parser_peek_char(struct kanshi_parser *parser) {
+static int parser_peek_char(struct kanshi_parser *parser) {
 	int ch = parser_read_char(parser);
 	parser->next = ch;
 	return ch;
 }
 
-bool parser_read_str(struct kanshi_parser *parser) {
+static bool parser_read_str(struct kanshi_parser *parser) {
 	while (1) {
 		int ch = parser_peek_char(parser);
 		if (ch < 0) {
@@ -79,7 +81,7 @@ bool parser_read_str(struct kanshi_parser *parser) {
 	}
 }
 
-bool parser_next_token(struct kanshi_parser *parser) {
+static bool parser_next_token(struct kanshi_parser *parser) {
 	while (1) {
 		int ch = parser_read_char(parser);
 		if (ch < 0) {
@@ -106,7 +108,7 @@ bool parser_next_token(struct kanshi_parser *parser) {
 	}
 }
 
-bool parser_expect_token(struct kanshi_parser *parser,
+static bool parser_expect_token(struct kanshi_parser *parser,
 		enum kanshi_token_type want) {
 	if (!parser_next_token(parser)) {
 		return false;
@@ -119,7 +121,24 @@ bool parser_expect_token(struct kanshi_parser *parser,
 	return true;
 }
 
-struct kanshi_profile_output *parse_profile_output(struct kanshi_parser *parser) {
+static bool parse_mode(struct kanshi_profile_output *output, const char *str) {
+	return true; // TODO
+}
+
+static bool parse_position(struct kanshi_profile_output *output, const char *str) {
+	return true; // TODO
+}
+
+static bool parse_float(float *dst, const char *str) {
+	return true; // TODO
+}
+
+static bool parse_transform(enum wl_output_transform *dst, const char *str) {
+	return true; // TODO
+}
+
+static struct kanshi_profile_output *parse_profile_output(
+		struct kanshi_parser *parser) {
 	struct kanshi_profile_output *output = calloc(1, sizeof(*output));
 
 	if (!parser_expect_token(parser, KANSHI_TOKEN_STR)) {
@@ -127,6 +146,8 @@ struct kanshi_profile_output *parse_profile_output(struct kanshi_parser *parser)
 	}
 	output->name = strdup(parser->tok_str);
 
+	bool has_key = false;
+	enum kanshi_output_field key;
 	while (1) {
 		if (!parser_next_token(parser)) {
 			return NULL;
@@ -134,7 +155,62 @@ struct kanshi_profile_output *parse_profile_output(struct kanshi_parser *parser)
 
 		switch (parser->tok_type) {
 		case KANSHI_TOKEN_STR:
-			// TODO
+			if (has_key) {
+				const char *value = parser->tok_str;
+				switch (key) {
+				case KANSHI_OUTPUT_MODE:
+					if (!parse_mode(output, value)) {
+						return NULL;
+					}
+					break;
+				case KANSHI_OUTPUT_POSITION:
+					if (!parse_position(output, value)) {
+						return NULL;
+					}
+					break;
+				case KANSHI_OUTPUT_SCALE:
+					if (!parse_float(&output->scale, value)) {
+						fprintf(stderr, "invalid output scale\n");
+						return NULL;
+					}
+					break;
+				case KANSHI_OUTPUT_TRANSFORM:
+					if (!parse_transform(&output->transform, value)) {
+						fprintf(stderr, "invalid output transform\n");
+						return NULL;
+					}
+					break;
+				default:
+					assert(0);
+				}
+				has_key = false;
+				output->fields |= key;
+			} else {
+				has_key = true;
+				const char *key_str = parser->tok_str;
+				if (strcmp(key_str, "enable") == 0) {
+					output->enabled = true;
+					output->fields |= KANSHI_OUTPUT_ENABLED;
+					has_key = false;
+				} else if (strcmp(key_str, "disable") == 0) {
+					output->enabled = false;
+					output->fields |= KANSHI_OUTPUT_ENABLED;
+					has_key = false;
+				} else if (strcmp(key_str, "mode") == 0) {
+					key = KANSHI_OUTPUT_MODE;
+				} else if (strcmp(key_str, "position") == 0) {
+					key = KANSHI_OUTPUT_POSITION;
+				} else if (strcmp(key_str, "scale") == 0) {
+					key = KANSHI_OUTPUT_SCALE;
+				} else if (strcmp(key_str, "transform") == 0) {
+					key = KANSHI_OUTPUT_TRANSFORM;
+				} else {
+					fprintf(stderr,
+						"unknown directive '%s' in profile output '%s'\n",
+						key_str, output->name);
+					return NULL;
+				}
+			}
 			break;
 		case KANSHI_TOKEN_NEWLINE:
 			return output;
@@ -146,7 +222,7 @@ struct kanshi_profile_output *parse_profile_output(struct kanshi_parser *parser)
 	}
 }
 
-struct kanshi_profile *parse_profile(struct kanshi_parser *parser) {
+static struct kanshi_profile *parse_profile(struct kanshi_parser *parser) {
 	if (!parser_expect_token(parser, KANSHI_TOKEN_LBRACKET)) {
 		return NULL;
 	}
@@ -162,8 +238,9 @@ struct kanshi_profile *parse_profile(struct kanshi_parser *parser) {
 		switch (parser->tok_type) {
 		case KANSHI_TOKEN_RBRACKET:
 			return profile;
-		case KANSHI_TOKEN_STR:
-			if (strcmp(parser->tok_str, "output") == 0) {
+		case KANSHI_TOKEN_STR:;
+			const char *directive = parser->tok_str;
+			if (strcmp(directive, "output") == 0) {
 				struct kanshi_profile_output *output =
 					parse_profile_output(parser);
 				if (output == NULL) {
@@ -171,8 +248,8 @@ struct kanshi_profile *parse_profile(struct kanshi_parser *parser) {
 				}
 				wl_list_insert(&profile->outputs, &output->link);
 			} else {
-				fprintf(stderr, "unexpected directive '%s' in profile\n",
-					parser->tok_str);
+				fprintf(stderr, "unknown directive '%s' in profile\n",
+					directive);
 				return NULL;
 			}
 			break;
@@ -186,7 +263,7 @@ struct kanshi_profile *parse_profile(struct kanshi_parser *parser) {
 	}
 }
 
-struct kanshi_config *_parse_config(struct kanshi_parser *parser) {
+static struct kanshi_config *_parse_config(struct kanshi_parser *parser) {
 	struct kanshi_config *config = calloc(1, sizeof(*config));
 	wl_list_init(&config->profiles);
 
