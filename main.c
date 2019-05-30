a6f0873e7c36322433eb3b4b078af1b775475daa
Simon Ser <contact@emersion.fr>
2019-05-30 23:47:56 +0300

    Listen to output-management events

--- main.c
+++ main.c
@@ -1,8 +1,199 @@
-#define _POSIX_C_SOURCE 1
+#define _POSIX_C_SOURCE 200809L
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
+#include <wayland-client.h>
 
+#include "kanshi.h"
 #include "parser.h"
+#include "wlr-output-management-unstable-v1-client-protocol.h"
+
+
+static void mode_handle_size(void *data, struct zwlr_output_mode_v1 *wlr_mode,
+		int32_t width, int32_t height) {
+	struct kanshi_mode *mode = data;
+	mode->width = width;
+	mode->height = height;
+}
+
+static void mode_handle_refresh(void *data,
+		struct zwlr_output_mode_v1 *wlr_mode, int32_t refresh) {
+	struct kanshi_mode *mode = data;
+	mode->refresh = refresh;
+}
+
+static void mode_handle_preferred(void *data,
+		struct zwlr_output_mode_v1 *wlr_mode) {
+	struct kanshi_mode *mode = data;
+	mode->preferred = true;
+}
+
+static void mode_handle_finished(void *data,
+		struct zwlr_output_mode_v1 *wlr_mode) {
+	struct kanshi_mode *mode = data;
+	wl_list_remove(&mode->link);
+	zwlr_output_mode_v1_destroy(mode->wlr_mode);
+	free(mode);
+}
+
+static const struct zwlr_output_mode_v1_listener mode_listener = {
+	.size = mode_handle_size,
+	.refresh = mode_handle_refresh,
+	.preferred = mode_handle_preferred,
+	.finished = mode_handle_finished,
+};
+
+static void head_handle_name(void *data,
+		struct zwlr_output_head_v1 *wlr_head, const char *name) {
+	struct kanshi_head *head = data;
+	head->name = strdup(name);
+}
+
+static void head_handle_description(void *data,
+		struct zwlr_output_head_v1 *wlr_head, const char *description) {
+	struct kanshi_head *head = data;
+	head->description = strdup(description);
+}
+
+static void head_handle_physical_size(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t width, int32_t height) {
+	struct kanshi_head *head = data;
+	head->phys_width = width;
+	head->phys_height = height;
+}
+
+static void head_handle_mode(void *data,
+		struct zwlr_output_head_v1 *wlr_head,
+		struct zwlr_output_mode_v1 *wlr_mode) {
+	struct kanshi_head *head = data;
+
+	struct kanshi_mode *mode = calloc(1, sizeof(*mode));
+	mode->head = head;
+	mode->wlr_mode = wlr_mode;
+	wl_list_insert(&head->modes, &mode->link);
+
+	zwlr_output_mode_v1_add_listener(wlr_mode, &mode_listener, mode);
+}
+
+static void head_handle_enabled(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t enabled) {
+	struct kanshi_head *head = data;
+	head->enabled = !!enabled;
+	if (!enabled) {
+		head->mode = NULL;
+	}
+}
+
+static void head_handle_current_mode(void *data,
+		struct zwlr_output_head_v1 *wlr_head,
+		struct zwlr_output_mode_v1 *wlr_mode) {
+	struct kanshi_head *head = data;
+	struct kanshi_mode *mode;
+	wl_list_for_each(mode, &head->modes, link) {
+		if (mode->wlr_mode == wlr_mode) {
+			head->mode = mode;
+			return;
+		}
+	}
+	fprintf(stderr, "received unknown current_mode\n");
+	head->mode = NULL;
+}
+
+static void head_handle_position(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t x, int32_t y) {
+	struct kanshi_head *head = data;
+	head->x = x;
+	head->y = y;
+}
+
+static void head_handle_transform(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t transform) {
+	struct kanshi_head *head = data;
+	head->transform = transform;
+}
+
+static void head_handle_scale(void *data,
+		struct zwlr_output_head_v1 *wlr_head, wl_fixed_t scale) {
+	struct kanshi_head *head = data;
+	head->scale = wl_fixed_to_double(scale);
+}
+
+static void head_handle_finished(void *data,
+		struct zwlr_output_head_v1 *wlr_head) {
+	struct kanshi_head *head = data;
+	wl_list_remove(&head->link);
+	zwlr_output_head_v1_destroy(head->wlr_head);
+	free(head->name);
+	free(head->description);
+	free(head);
+}
+
+static const struct zwlr_output_head_v1_listener head_listener = {
+	.name = head_handle_name,
+	.description = head_handle_description,
+	.physical_size = head_handle_physical_size,
+	.mode = head_handle_mode,
+	.enabled = head_handle_enabled,
+	.current_mode = head_handle_current_mode,
+	.position = head_handle_position,
+	.transform = head_handle_transform,
+	.scale = head_handle_scale,
+	.finished = head_handle_finished,
+};
+
+static void output_manager_handle_head(void *data,
+		struct zwlr_output_manager_v1 *manager,
+		struct zwlr_output_head_v1 *wlr_head) {
+	struct kanshi_state *state = data;
+
+	struct kanshi_head *head = calloc(1, sizeof(*head));
+	head->state = state;
+	head->wlr_head = wlr_head;
+	head->scale = 1.0;
+	wl_list_init(&head->modes);
+	wl_list_insert(&state->heads, &head->link);
+
+	zwlr_output_head_v1_add_listener(wlr_head, &head_listener, head);
+}
+
+static void output_manager_handle_done(void *data,
+		struct zwlr_output_manager_v1 *manager, uint32_t serial) {
+	struct kanshi_state *state = data;
+	state->serial = serial;
+}
+
+static void output_manager_handle_finished(void *data,
+		struct zwlr_output_manager_v1 *manager) {
+	// This space is intentionally left blank
+}
+
+static const struct zwlr_output_manager_v1_listener output_manager_listener = {
+	.head = output_manager_handle_head,
+	.done = output_manager_handle_done,
+	.finished = output_manager_handle_finished,
+};
+
+static void registry_handle_global(void *data, struct wl_registry *registry,
+		uint32_t name, const char *interface, uint32_t version) {
+	struct kanshi_state *state = data;
+
+	if (strcmp(interface, zwlr_output_manager_v1_interface.name) == 0) {
+		state->output_manager = wl_registry_bind(registry, name,
+			&zwlr_output_manager_v1_interface, 1);
+		zwlr_output_manager_v1_add_listener(state->output_manager,
+			&output_manager_listener, state);
+	}
+}
+
+static void registry_handle_global_remove(void *data,
+		struct wl_registry *registry, uint32_t name) {
+	// This space is intentionally left blank
+}
+
+static const struct wl_registry_listener registry_listener = {
+	.global = registry_handle_global,
+	.global_remove = registry_handle_global_remove,
+};
 
 int main(int argc, char *argv[]) {
 	const char config_filename[] = "kanshi/config";
@@ -23,6 +214,32 @@ int main(int argc, char *argv[]) {
 	struct kanshi_config *config = parse_config(config_path);
 	if (config == NULL) {
 		return EXIT_FAILURE;
+	}
+
+	struct wl_display *display = wl_display_connect(NULL);
+	if (display == NULL) {
+		fprintf(stderr, "failed to connect to display\n");
+		return EXIT_FAILURE;
+	}
+
+	struct kanshi_state state = {
+		.running = true,
+	};
+	wl_list_init(&state.heads);
+
+	struct wl_registry *registry = wl_display_get_registry(display);
+	wl_registry_add_listener(registry, &registry_listener, &state);
+	wl_display_dispatch(display);
+	wl_display_roundtrip(display);
+
+	if (state.output_manager == NULL) {
+		fprintf(stderr, "compositor doesn't support "
+			"wlr-output-management-unstable-v1\n");
+		return EXIT_FAILURE;
+	}
+
+	while (state.running && wl_display_dispatch(display) != -1) {
+		// This space intentionally left blank
 	}
 
 	return EXIT_SUCCESS;
